cd6c9ee823faa2a83fd161ecc55923738eb3f668
roidrole <roidorle@gmail.com>
2025-09-24 13:39:04 -0400

    indexOf indexOf

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 
 
 int findChar(char word[], char character){
-    for (int i = 0; i < sizeof(word); i++){
+    for (int i = 0; word[i] != 0; i++){
         if(word[i] == character){
             return i;
         }
     }
     return -1;
 }
+
 int main(){
     printf("%d", findChar("you_mom_is_hat", 'a'));
 }
